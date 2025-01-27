0e6f01813dfce70e9e27ce1b77a68345542a0bc4
城市星表_之音饲料馆 <155783942+1028867764@users.noreply.github.com>
2025-01-28 07:46:25 +0800

    我已崩溃

--- js/nav.js
+++ js/nav.js
@@ -1,13 +1,13 @@
-$(function(){
+$(function () {
     // nav收缩展开
-    $('.nav-item>a').on('click',function(){
+    $('.nav-item>a').on('click', function () {
         if (!$('.nav').hasClass('nav-mini')) {
             if ($(this).next().css('display') == "none") {
                 //展开未展开
                 $('.nav-item').children('ul').slideUp(300);
                 $(this).next('ul').slideDown(300);
                 $(this).parent('li').addClass('nav-show').siblings('li').removeClass('nav-show');
-            }else{
+            } else {
                 //收缩已展开
                 $(this).next('ul').slideUp(300);
                 $('.nav-item.nav-show').removeClass('nav-show');
@@ -15,13 +15,15 @@ $(function(){
         }
     });
     //nav-mini切换
-    $('#mini').on('click',function(){
+    $('#mini').on('click', function () {
         if (!$('.nav').hasClass('nav-mini')) {
             $('.nav-item.nav-show').removeClass('nav-show');
             $('.nav-item').children('ul').removeAttr('style');
             $('.nav').addClass('nav-mini');
-        }else{
+        } else {
             $('.nav').removeClass('nav-mini');
         }
     });
+    //我写的新代码
+    
 });
